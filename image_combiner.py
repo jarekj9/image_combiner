fff6887c709f38e2fceb0647714da78b8211ea1f
Jaroslaw <jerry.esp.sha@gmail.com>
2021-10-08 13:51:29 +0200

    change arguments and class methods from class to standard

--- image_combiner.py
+++ image_combiner.py
@@ -12,27 +12,26 @@ from gooey import Gooey
 
 class ImageCombiner:
 
-    SEPARATOR_WIDTH = 20
-    SEPARATOR_COLOR = (0, 0, 0)
+    def __init__(self, separator_color, separator_width):
+        self.separator_color = separator_color
+        self.separator_width = separator_width
 
-    @classmethod
-    def combine_horizontal(cls, im1, im2):
+    def combine_horizontal(self, im1, im2):
         '''Combine images horizontally'''
-        combined_im = Image.new('RGB', (im1.width + im2.width + cls.SEPARATOR_WIDTH, max(im1.height, im2.height)))
-        separator_im = Image.new("RGB", (cls.SEPARATOR_WIDTH, max(im1.height, im2.height)), cls.SEPARATOR_COLOR)
+        combined_im = Image.new('RGB', (im1.width + im2.width + self.separator_width, max(im1.height, im2.height)))
+        separator_im = Image.new("RGB", (self.separator_width, max(im1.height, im2.height)), self.separator_color)
         combined_im.paste(im1, (0, 0))
         combined_im.paste(separator_im, (im1.width, 0))
-        combined_im.paste(im2, (im1.width + cls.SEPARATOR_WIDTH, 0))
+        combined_im.paste(im2, (im1.width + self.separator_width, 0))
         return combined_im
 
-    @classmethod
-    def combine_vertical(cls, im1, im2):
+    def combine_vertical(self, im1, im2):
         '''Combine images vertically'''
-        combined_im = Image.new('RGB', (max(im1.width, im2.width), im1.height + im2.height + cls.SEPARATOR_WIDTH))
-        separator_im = Image.new("RGB", (max(im1.width, im2.width), cls.SEPARATOR_WIDTH), cls.SEPARATOR_COLOR)
+        combined_im = Image.new('RGB', (max(im1.width, im2.width), im1.height + im2.height + self.separator_width))
+        separator_im = Image.new("RGB", (max(im1.width, im2.width), self.separator_width), self.separator_color)
         combined_im.paste(im1, (0, 0))
         combined_im.paste(separator_im, (0, im1.height))
-        combined_im.paste(im2, (0, im1.height + cls.SEPARATOR_WIDTH))
+        combined_im.paste(im2, (0, im1.height + self.separator_width))
         return combined_im
 
 def give_filenames(name):
@@ -48,28 +47,27 @@ def give_filenames(name):
 @Gooey
 def main():
     parser = argparse.ArgumentParser(usage='Choose arguments.')
-    parser.add_argument('--vertical', action="store_true", help='Combine images vertically')
-    parser.add_argument('--horizontal', action="store_true", help='Combine images horizontally')
+    parser.add_argument("--method", choices=["vertical", "horizontal"], default="vertical", help="Choose combine method: vertical or horizontal.")
     parser.add_argument("--separator_color", choices=["black", "red", "blue"], default="black", help="Choose separator color.")
     parser.add_argument('--separator_width', type=int, default=20, help='Choose separator width.')
     parser.add_argument('--version','-v', action='version', version='%(prog)s 1.0')
     args = parser.parse_args()
 
-    if args.vertical:
-        combine_method = lambda im1, im2: ImageCombiner.combine_vertical(im1, im2)
-    elif args.horizontal:
-        combine_method = lambda im1, im2: ImageCombiner.combine_horizontal(im1, im2)
-    else:
-        combine_method = lambda im1, im2: ImageCombiner.combine_vertical(im1, im2)
-
     if args.separator_color == 'black':
-        ImageCombiner.SEPARATOR_COLOR = (0, 0 ,0)
+        separator_color = (0, 0, 0)
     elif args.separator_color == 'red':
-        ImageCombiner.SEPARATOR_COLOR = (255, 0 ,0)
+        separator_color = (255, 0, 0)
     elif args.separator_color == 'blue':
-        ImageCombiner.SEPARATOR_COLOR = (0,0,255)
+        separator_color = (0, 0, 255)
 
-    ImageCombiner.SEPARATOR_WIDTH = args.separator_width
+    combiner = ImageCombiner(separator_color, args.separator_width)
+
+    if args.method == "vertical":
+        combine_method = lambda im1, im2: combiner.combine_vertical(im1, im2)
+    elif args.method == "horizontal":
+        combine_method = lambda im1, im2: combiner.combine_horizontal(im1, im2)
+    else:
+        combine_method = lambda im1, im2: combiner.combine_vertical(im1, im2)
 
     files = []
     for format in ['jpg', 'jpeg', 'png', 'bmp', 'gif']:
